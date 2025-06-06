789a1c1903f6e7d0ee8012bf0f8aa522cb0d66d7
Emiliano0041 <emilianosalguero31@gmail.com>
2025-06-06 13:46:01 +0000

    En curso

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -9,22 +9,72 @@ SELECT * FROM observations;
 
 
 -- MISSION 1
--- Your query here
+-- Your query here;
+--**Misión 1:** Queremos conocer la biodiversidad de cada región. ¿Qué regiones tienen más especies registradas?;
+SELECT 
+    regions.name,
+    regions.country,
+    COUNT(DISTINCT species.id) AS total_species
+FROM observations
+JOIN species ON observations.species_id = species.id
+JOIN regions ON observations.region_id = regions.id
+GROUP BY regions.name
+ORDER BY total_species DESC;
+
+
 
 -- MISSION 2
 -- Your query here:
+--¿Qué meses tienen mayor actividad de observación? Agrupa por mes a partir de las fechas de observación reales. Es útil para detectar estacionalidad;
+SELECT 
+    strftime('%m', observations.observation_date) AS month,
+    COUNT(*) AS total
+FROM observations
+GROUP BY month
+ORDER BY total DESC;
 
 
 -- MISSION 3
--- Your query here:
+-- Your query here
+--Detecta las especies con pocos individuos registrados (posibles casos raros);
+SELECT species.scientific_name, species.common_name, SUM(observations.count) AS total_count
+FROM species
+JOIN observations ON species.id = observations.species_id
+GROUP BY scientific_name
+HAVING total_count < 3;
 
 
--- MISSION 4
--- Your query here:
 
 
--- MISSION 5
--- Your query here:
+
+
+-- MISSION 4 ¿Qué región tiene el mayor número de especies distintas observadas?;
+
+SELECT 
+    regions.name,
+    regions.country,
+    COUNT(DISTINCT species.id) AS total_species
+FROM observations
+JOIN species ON observations.species_id = species.id
+JOIN regions ON observations.region_id = regions.id
+GROUP BY regions.name
+ORDER BY total_species DESC
+LIMIT 1;
+ 
+
+
+-- MISSION 5 ¿Qué especies han sido observadas con mayor frecuencia?
+-- Your query here:;
+SELECT  species.id, species.scientific_name, species.common_name, SUM(observations.count) AS species_frecuencia
+FROM observations
+JOIN species ON observations.species_id = species.id
+GROUP BY species.id
+ORDER BY species_frecuencia DESC
+
+;
+
+
+
 
 
 -- MISSION 6
